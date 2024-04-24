51d43c43ade617dc4970b95abd76f2af5c0abf24
orjuela <orjuela@MateLinux643.myguest.virtualbox.org>
2024-04-24 09:46:08 -0500

    build route traking view

--- src/main/kotlin/org/example/RouteTrackingController.kt
+++ src/main/kotlin/org/example/RouteTrackingController.kt
@@ -3,6 +3,7 @@ package org.example
 import javafx.fxml.FXML
 import javafx.fxml.Initializable
 import javafx.scene.control.Button
+import javafx.scene.control.Label
 import javafx.scene.image.Image
 import javafx.scene.image.ImageView
 import java.net.URL
@@ -11,9 +12,27 @@ import java.util.*
 class RouteTrackingController: Initializable {
 
     @FXML var imageView: ImageView? = null
+
+    @FXML var driverNameLabel: Label? = null
+    @FXML var routeCodeLabel: Label? = null
+    @FXML var nextStopNameLabel: Label? = null
+
     @FXML var nextBusETAImageView: ImageView? = null
+    @FXML var nextBusETALabel: Label? = null
+
     @FXML var currentBusETAImageView: ImageView? = null
+    @FXML var currentBusETALabel: Label? = null
+
     @FXML var previousBusETAImageView: ImageView? = null
+    @FXML var previousBusETALabel: Label? = null
+
+    @FXML var initialHourLabel: Label? = null
+    @FXML var finalHourLabel: Label? = null
+    @FXML var timeLabel: Label? = null
+
+    @FXML var nextStopLabel: Label? = null
+    @FXML var speedLabel: Label? = null
+    @FXML var nextStopDistanceLabel: Label? = null
 
     @FXML var closeRouteTrackingFragmentButton: Button? = null
 
